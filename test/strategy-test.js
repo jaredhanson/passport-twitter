fdd51486d665e3d50412b1e9e202a7d197e9b5f2
Jared Hanson <jaredhanson@gmail.com>
2012-03-31 17:41:37 -0700

    Wrap error for more informative messages.

--- test/strategy-test.js
+++ test/strategy-test.js
@@ -99,6 +99,9 @@ vows.describe('TwitterStrategy').addBatch({
       'should error' : function(err, req) {
         assert.isNotNull(err);
       },
+      'should wrap error in InternalOAuthError' : function(err, req) {
+        assert.equal(err.constructor.name, 'InternalOAuthError');
+      },
       'should not load profile' : function(err, profile) {
         assert.isUndefined(profile);
       },
