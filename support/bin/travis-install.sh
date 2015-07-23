58927d0a7f1f8f01088cd5b08da39a9668ecee1f
Jared Hanson <jaredhanson@gmail.com>
2015-07-23 12:51:19 -0700

    istanbul uprev to 0.3.8 for Travis CI.

--- support/bin/travis-install.sh
+++ support/bin/travis-install.sh
@@ -6,7 +6,7 @@ echo "Node version: ${nodeVersion}"
 case $nodeVersion in
   v0.8.*)
     echo "Node 0.8"
-    npm install istanbul@0.3.6 -g
+    npm install istanbul@0.3.8 -g
     npm install coveralls -g
     ;;
   *) # latest
