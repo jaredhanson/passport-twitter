d570b19181897da020ef1f72de2a31a617e7fb4a
Jared Hanson <jaredhanson@gmail.com>
2016-01-29 10:03:23 -0800

    Error test cases.

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -35,4 +35,70 @@ describe('Strategy', function() {
     });
   });
   
+  describe('failure cause by invalid consumer secret sent to request token URL', function() {
+    var strategy = new TwitterStrategy({
+      consumerKey: 'ABC123',
+      consumerSecret: 'invalid-secret',
+      callbackURL: 'http://www.example.test/invalid-callback'
+    }, function verify(){});
+    
+    strategy._oauth.getOAuthRequestToken = function(params, callback) {
+      callback({ statusCode: 401, data: '{"errors":[{"code":32,"message":"Could not authenticate you."}]}' });
+    }
+    
+    
+    var err;
+  
+    before(function(done) {
+      chai.passport(strategy)
+        .error(function(e) {
+          err = e;
+          done();
+        })
+        .req(function(req) {
+          req.query = {};
+          req.session = {};
+        })
+        .authenticate();
+    });
+  
+    it('should error', function() {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.message).to.equal("Could not authenticate you.");
+    });
+  });
+  
+  describe('failure cause by invalid callback sent to request token URL', function() {
+    var strategy = new TwitterStrategy({
+      consumerKey: 'ABC123',
+      consumerSecret: 'secret',
+      callbackURL: 'http://www.example.test/invalid-callback'
+    }, function verify(){});
+    
+    strategy._oauth.getOAuthRequestToken = function(params, callback) {
+      callback({ statusCode: 401, data: '<?xml version="1.0" encoding="UTF-8"?>\n<hash>\n  <error>This client application\'s callback url has been locked</error>\n  <request>/oauth/request_token</request>\n</hash>\n' });
+    }
+    
+    
+    var err;
+  
+    before(function(done) {
+      chai.passport(strategy)
+        .error(function(e) {
+          err = e;
+          done();
+        })
+        .req(function(req) {
+          req.query = {};
+          req.session = {};
+        })
+        .authenticate();
+    });
+  
+    it('should error', function() {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.message).to.equal("This client application's callback url has been locked");
+    });
+  });
+  
 });
